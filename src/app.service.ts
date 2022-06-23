ffdd59a78c61f8c2626ed6e265f5b90cf5fb70aa
Sajjad Shojaei Moghaddam <sajjadshojaeimoghaddam@gmail.com>
2022-06-23 12:43:47 +0430

    update

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Hears, Help, On, Start, Update } from 'nestjs-telegraf';
+import { Command, Hears, Help, On, Start, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 
 @Update()
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!!';   
+    return 'Hello World!!';
   }
 
   @Start()
@@ -26,6 +26,24 @@ export class AppService {
   @Hears('hi')
   async hearsHi(ctx: Context) {
     await ctx.reply('Hey there');
+  }
+
+  @Command('inline')
+  async inlineCommand(ctx: Context){
+    ctx.reply("Hi there!", {
+      reply_markup: {
+        inline_keyboard: [
+          /* Inline buttons. 2 side-by-side */
+          [{ text: "Button 1", callback_data: "btn-1" }, { text: "Button 2", callback_data: "btn-2" }],
+  
+          /* One button */
+          [{ text: "Next", callback_data: "next" }],
+  
+          /* Also, we can have URL buttons. */
+          [{ text: "Open in browser", url: "telegraf.js.org" }]
+        ]
+      }
+    });
   }
 
 }
